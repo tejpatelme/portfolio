3405c7c91844030ff9a09ebde2e0860a47670918
Tej <pateltej.me@gmail.com>
2025-04-18 09:34:14 +0530

    feat: v1 portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site:"https://tejpatel.in",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [icon(), sitemap()],
+  build: {
+    inlineStylesheets: "always"
+  }
+});
